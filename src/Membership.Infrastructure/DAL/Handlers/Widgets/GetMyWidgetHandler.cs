48c3fdd79a38d528c6654ebfa9a7f4b54ce82e62
rafimk <rafi.genius.cs@gmail.com>
2022-08-06 12:43:55 +0400

    Widget implimentation

--- src/Membership.Infrastructure/DAL/Handlers/Widgets/GetMyWidgetHandler.cs
+++ src/Membership.Infrastructure/DAL/Handlers/Widgets/GetMyWidgetHandler.cs
@@ -46,6 +46,7 @@ internal sealed class GetMyWidgetHandler : IQueryHandler<GetMyWidget, IEnumerabl
         
         if (user.Role == UserRole.CentralCommitteeAdmin())
         {
+            // ====================== Users ======================
             var userCountByState = await _dbContext.Users
                 .Include(x => x.State)
                 .Where(x => x.Role == UserRole.StateAdmin())
@@ -53,13 +54,13 @@ internal sealed class GetMyWidgetHandler : IQueryHandler<GetMyWidget, IEnumerabl
                 .Select(x => new { StateId = x.Key, StateName = x.First().State.Name, Count = x.Count() })
                 .ToListAsync();
             
-            Int32 totalUserCount = 0; 
+            Int32 totalStateAdminUserCount = 0; 
             
             foreach(var item in userCountByState)
             {
                 if (item.StateName is not null)
                 {
-                    totalUserCount += item.Count;
+                    totalStateAdminUserCount += item.Count;
                     widgetDetails.Add(new WidgetDetailDto
                     {
                         Text = item.StateName,
@@ -73,7 +74,40 @@ internal sealed class GetMyWidgetHandler : IQueryHandler<GetMyWidget, IEnumerabl
             {
                 No = 1,
                 Title = "Total Users",
-                SummaryValue = totalUserCount,
+                SummaryValue = totalStateAdminUserCount,
+                SummaryText = null,
+                Details = widgetDetails
+            });
+            
+            // ====================== Members ======================
+            var memberCountByState = await _dbContext.Members
+                .Include(x => x.State)
+                .GroupBy(x => x.StateId)
+                .Select(x => new { StateId = x.Key, StateName = x.First().State.Name, Count = x.Count() })
+                .ToListAsync();
+            
+            Int32 totalStateMemberCount = 0; 
+            widgetDetails= new();
+            
+            foreach(var item in memberCountByState)
+            {
+                if (item.StateName is not null)
+                {
+                    totalStateMemberCount += item.Count;
+                    widgetDetails.Add(new WidgetDetailDto
+                    {
+                        Text = item.StateName,
+                        IntValue = item.Count,
+                        TextValue = null
+                    });
+                }
+            }
+            
+            widget.Add(new WidgetDto
+            {
+                No = 1,
+                Title = "Total Members",
+                SummaryValue = totalStateMemberCount,
                 SummaryText = null,
                 Details = widgetDetails
             });
@@ -83,9 +117,10 @@ internal sealed class GetMyWidgetHandler : IQueryHandler<GetMyWidget, IEnumerabl
         
         if (user.Role == UserRole.StateAdmin())
         {
+            // ====================== Users ======================
             var userCountByDistrict = await _dbContext.Users
                 .Include(x => x.District)
-                .Where(x => x.Role == UserRole.DistrictAdmin() || x.Role == UserRole.DistrictAgent())
+                .Where(x => x.StateId == user.StateId && (x.Role == UserRole.DistrictAdmin() || x.Role == UserRole.DistrictAgent()))
                 .GroupBy(x => x.DistrictId)
                 .Select(x => new { DistrictId = x.Key, DistrictName = x.First().District.Name, Count = x.Count() })
                 .ToListAsync();
@@ -114,15 +149,50 @@ internal sealed class GetMyWidgetHandler : IQueryHandler<GetMyWidget, IEnumerabl
                 SummaryText = null,
                 Details = widgetDetails
             });
+            
+            // ====================== Members ======================
+            var memberCountByDistrict = await _dbContext.Members
+                .Include(x => x.District)
+                .Where(x => x.StateId == user.StateId)
+                .GroupBy(x => x.DistrictId)
+                .Select(x => new { DistrictId = x.Key, DistrictName = x.First().District.Name, Count = x.Count() })
+                .ToListAsync();
+            
+            Int32 totalDistrictMemberCount = 0; 
+            widgetDetails= new();
+            
+            foreach(var item in memberCountByDistrict)
+            {
+                if (item.DistrictName is not null)
+                {
+                    totalDistrictMemberCount += item.Count;
+                    widgetDetails.Add(new WidgetDetailDto
+                    {
+                        Text = item.DistrictName,
+                        IntValue = item.Count,
+                        TextValue = null
+                    });
+                }
+            }
+            
+            widget.Add(new WidgetDto
+            {
+                No = 1,
+                Title = "Total Members",
+                SummaryValue = totalDistrictMemberCount,
+                SummaryText = null,
+                Details = widgetDetails
+            });
 
             return widget;
         }
         
         if (user.Role == UserRole.DistrictAdmin())
         {
+            // ====================== Users ======================
             var userCountByMandalam = await _dbContext.Users
                 .Include(x => x.Mandalam)
-                .Where(x => x.Role == UserRole.MandalamAgent())
+                .Where(x => x.StateId == user.StateId && x.DistrictId == user.DistrictId && x.Role == UserRole.MandalamAgent())
                 .GroupBy(x => x.MandalamId)
                 .Select(x => new { MandalamId = x.Key, MandalamName = x.First().Mandalam.Name, Count = x.Count() })
                 .ToListAsync();
@@ -151,32 +221,122 @@ internal sealed class GetMyWidgetHandler : IQueryHandler<GetMyWidget, IEnumerabl
                 SummaryText = null,
                 Details = widgetDetails
             });
+            
+            // ====================== Members ======================
+            var memberCountByMandalam = await _dbContext.Members
+                .Include(x => x.Mandalam)
+                .Where(x => x.StateId == user.StateId && x.DistrictId == user.DistrictId)
+                .GroupBy(x => x.MandalamId)
+                .Select(x => new { MandalamId = x.Key, MandalamName = x.First().Mandalam.Name, Count = x.Count() })
+                .ToListAsync();
+            
+            Int32 totalMandalamMemberCount = 0; 
+            widgetDetails= new();
+            
+            foreach(var item in memberCountByMandalam)
+            {
+                if (item.MandalamName is not null)
+                {
+                    totalMandalamMemberCount += item.Count;
+                    widgetDetails.Add(new WidgetDetailDto
+                    {
+                        Text = item.MandalamName,
+                        IntValue = item.Count,
+                        TextValue = null
+                    });
+                }
+            }
+            
+            widget.Add(new WidgetDto
+            {
+                No = 1,
+                Title = "Total Members",
+                SummaryValue = totalMandalamMemberCount,
+                SummaryText = null,
+                Details = widgetDetails
+            });
 
             return widget;
         }
 
-        var availableUsers = await _dbContext.Users
-            .OrderBy(x => x.FullName)
-            .AsNoTracking()
-            .Select(x => x.AsDto())
-            .ToListAsync();
-        
-        var userDataRetrievepolicies = _userDataRetrievepolicies.SingleOrDefault(x => x.CanBeApplied(user.Role));
-
-        if (userDataRetrievepolicies is null)
+        if (user.Role == UserRole.DistrictAgent())
         {
-            throw new NoUserDataReteivePolicyFoundException(user.Role.ToString());
+            // ====================== Members ======================
+            var memberCountByMandalam = await _dbContext.Members
+                .Include(x => x.Mandalam)
+                .Where(x => x.StateId == user.StateId && x.DistrictId == user.DistrictId)
+                .GroupBy(x => x.MandalamId)
+                .Select(x => new { MandalamId = x.Key, MandalamName = x.First().Mandalam.Name, Count = x.Count() })
+                .ToListAsync();
+            
+            Int32 totalMandalamMemberCount = 0; 
+            widgetDetails= new();
+            
+            foreach(var item in memberCountByMandalam)
+            {
+                if (item.MandalamName is not null)
+                {
+                    totalMandalamMemberCount += item.Count;
+                    widgetDetails.Add(new WidgetDetailDto
+                    {
+                        Text = item.MandalamName,
+                        IntValue = item.Count,
+                        TextValue = null
+                    });
+                }
+            }
+            
+            widget.Add(new WidgetDto
+            {
+                No = 1,
+                Title = "Total Members",
+                SummaryValue = totalMandalamMemberCount,
+                SummaryText = null,
+                Details = widgetDetails
+            });
+
+            return widget;
         }
         
-        var users = userDataRetrievepolicies.GetData(user.Role, availableUsers, applicableUserRoles, user.StateId, user.DistrictId);
-
-        var widgetPolicies = _widgetPolicies.SingleOrDefault(x => x.CanBeApplied(user.Role));
-
-        if (widgetPolicies is null)
+        if (user.Role == UserRole.MandalamAgent())
         {
-            throw new NoWidgetPolicyFoundException(user.Role.ToString());
+            // ====================== Members ======================
+            var memberCountByPanchayat = await _dbContext.Members
+                .Include(x => x.Panchayat)
+                .Where(x => x.StateId == user.StateId && x.DistrictId == user.DistrictId)
+                .GroupBy(x => x.PanchayatId)
+                .Select(x => new { PanchayatId = x.Key, PanchayatName = x.First().Panchayat.Name, Count = x.Count() })
+                .ToListAsync();
+            
+            Int32 totalPanchayatMemberCount = 0; 
+            widgetDetails= new();
+            
+            foreach(var item in memberCountByPanchayat)
+            {
+                if (item.PanchayatName is not null)
+                {
+                    totalPanchayatMemberCount += item.Count;
+                    widgetDetails.Add(new WidgetDetailDto
+                    {
+                        Text = item.PanchayatName,
+                        IntValue = item.Count,
+                        TextValue = null
+                    });
+                }
+            }
+            
+            widget.Add(new WidgetDto
+            {
+                No = 1,
+                Title = "Total Members",
+                SummaryValue = totalPanchayatMemberCount,
+                SummaryText = null,
+                Details = widgetDetails
+            });
+
+            return widget;
         }
 
-        return widgetPolicies.GetData(user.Role, users, new List<MemberDto>());
+        return widget;
     }
 }
